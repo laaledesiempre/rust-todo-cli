c2b501e923a6a4a47d64bf54ae8ded5da4c4534e
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2024-02-03 10:01:36 -0300

    added todo and file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+struct Todo {
+    list: String;
+    todo: String;
+}
+
 fn main() {
     let args: Vec<String>= std::env::args().collect();
 
@@ -7,5 +12,34 @@ fn main() {
     } else {
         println!("not enough args");
     }
+}
 
+// Todo manipulation
+fn todo_get(list: String) {
+    todo!("List all todos on a list")
+}
+
+fn todo_add(todo: Todo) {
+    todo!("Adds a todo to a list")
+}
+
+fn todo_mark_as_done(todo: Todo){
+    todo!("Mark as done a todo")
+}
+
+fn todo_delete(todo: Todo){
+    todo!("Delete a todo from the list and renumber all the other todos")
+}
+
+// List manipulation
+fn list_get() {
+    todo!("List all lists")
+}
+
+fn list_add(list_name:String) {
+    todo!("Creates a new list")
+}
+
+fn list_remove(list_name:String) {
+    todo!("Remove a whole list")
 }
