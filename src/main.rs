cfe8c8b803e126a9eed4e0f02c877af046964e37
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2024-02-07 14:34:40 -0300

    added more code to the parser

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ enum Action {
     mark,
     get,
 }
-
+#[derive(Debug)]
 struct Todo_action {
     action: Action,
     list: Option<String>,
@@ -15,34 +15,74 @@ fn main() {
     let args: Vec<String>= std::env::args().collect();
 
     let todo_action = parse_args(&args);
-    //println!("{:?}",args);
-    if args[1] == "add" && args.len() >2 {
-        println!("Added {}",args[2])
-    } else {
-        println!("not enough args");
-    }
+
 }
 
 // Args manipulation
 
-fn parse_args(args: &Vec<String>)-> Todo_action {
+fn parse_args(args: &Vec<String>)-> Result<Todo_action,String> {
     match args.len() {
         2=>{
             match args[1] { // $ todo get
-                String::from("get")=> Todo_action {
+                String::from("get")=> Ok(Todo_action {
                     action: Action::get,
                     list: None,
                     todo: None
-                }
-                _=>println!("Can't make a one long with {}!",args[1]);
+                })
+                _=>Err(format!("Can't make a one long with {}!",args[1]));//TODO
             }
         }, 
         3=> {
-            todo!("Parse to remove/add list");
+            match args[1] {
+                String::from("add")=> Ok(Todo_action {
+                    action: Action::add,
+                    list: args[2],
+                    todo: None,
+                }),
+                String::from("remove")=> Ok(Todo_action {
+                    action: Action::remove,
+                    list: args[2],
+                    todo: None,
+                }),
+                String::from("mark")=> Ok(Todo_action {
+                    action: Action::mark,
+                    list: args[2],
+                    todo: None,       
+                }),
+                String::from("get")=> Ok(Todo_action {
+                    action: Action::get,
+                    list: args[2],
+                    todo: None
+                }),
+                _=>Err(format!("Can't make a one long with {}!",args[1]));//TODO
+            }
         },
         4=>{
-            todo!("Parse the rest of situations");
+            match args[1] {
+                 String::from("add")=> Ok(Todo_action {
+                    action: Action::add,
+                    list: args[2],
+                    todo: args[3],
+                }),
+                String::from("remove")=> Ok(Todo_action {
+                    action: Action::remove,
+                    list: args[2],
+                    todo: args[3],
+                }),
+                String::from("mark")=> Ok(Todo_action {
+                    action: Action::mark,
+                    list: args[2],
+                    todo: args[3],       
+                }),
+                String::from("get")=> Ok(Todo_action {
+                    action: Action::get,
+                    list: args[2],
+                    todo: args[3],
+                }),
+                _=>Err(format!("Can't make a one long with {}!",args[1]));//TODO               
+            }
         }
+        _=>Err(String::from("Not one of the program options"))
     }
 }
 
