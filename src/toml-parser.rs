e063269a51ed3572b2d69cacbb63a9365299b917
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2024-02-14 15:38:04 -0300

    added global values support

--- src/toml-parser.rs
+++ src/toml-parser.rs
@@ -1,38 +1,48 @@
 fn main() {
     #[derive(Debug)]
-    struct TomlValue {
+    struct TomlFile{
+        globals: Vec<TomlValue>,
+        tables: Vec<TomlTable>,
+    }
+    #[derive(Debug)]
+    struct TomlValue{
         key:String,
         value:String,
     }
-
     #[derive(Debug)]
     struct TomlTable {
         name: String,
         values: Vec<TomlValue>
     }
     
-    let text = "[ho]\na=how\nb=are\nc=you\n   \n[ho]\na=how\nb=are\nc=you\n";
-
-    let mut currentTable= TomlTable { name:"".to_string(), values: vec![]};
+    let text = "[ho]\na=how\nb=are\nc=you\n   \n[he]\na=how\nb=are\nc=you\n";
+    let mut current_file=TomlFile {globals:vec![], tables:vec![]};
+    let mut current_table_index= 0;
     
     for x in text.lines() {
         let x=x.trim();
         match x {
-            x if x.contains("[")=>{
-                println!("class name: {}",x);
-                currentTable.name=x.to_string().replace("[","").replace("]","");
-            },
             x if x.contains("=")=>{
-                let key_value:Vec<&str>= x.split("=").collect();
-                println!("key: {}, value: {}",key_value[0],key_value[1]);
-                currentTable.values.push(TomlValue {key: key_value[0].to_string(),value: key_value[1].to_string()});
+                if current_table_index==0 {
+                    let key_value:Vec<&str>= x.split("=").collect();
+                    current_file.globals.push(TomlValue {key: key_value[0].to_string(),value: key_value[1].to_string()})
+                } else {
+                    let key_value:Vec<&str>= x.split("=").collect();
+                    current_file.tables[current_table_index-1].values.push(TomlValue {key: key_value[0].to_string(),value: key_value[1].to_string()});
+                }    
+            },
+            x if x.contains("[")=>{
+                let name =x.to_string().replace("[","").replace("]","");
+                current_file.tables.push(TomlTable {name:name, values: vec![]});
+                current_table_index=current_table_index+1;
+    
             },
             x if (x=="")=> {
-                println!("end of table {}",x);
+                println!("empty line {}",x);
             },
-            _=>panic!("non valid file {}",x)
+            _=>panic!("non valid value {}",x)
             
         }
     }
-    println!("{:?}",currentTable)
-    }
+    println!("{:?}",current_file)
+}
